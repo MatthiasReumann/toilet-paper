2bb00defc4c93cd052d779305009e1339ddf7666
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2021-09-22 11:23:30 +0200

    Add createdAt to Purchase

--- Sources/App/Controllers/Contents/Purchase.swift
+++ Sources/App/Controllers/Contents/Purchase.swift
@@ -7,11 +7,13 @@ final class Purchase: Content {
     var name: String
     var priceInCent: Int
     var amount: Int
+    var createdAt: Date
     
     init(name: String, priceInCent: Int, amount: Int){
         self.id = UUID.generateRandom()
         self.name = name
         self.priceInCent = priceInCent
         self.amount = amount
+        self.createdAt = Date.init()
     }
 }
